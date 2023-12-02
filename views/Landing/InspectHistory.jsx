36ca2ffea1f785e336cccd932fdb441d2a8a6330
Meljohnzer <meljohnzer.hamoc@gmail.com>
2023-12-02 14:27:46 +0800

    Icon

--- views/Landing/InspectHistory.jsx
+++ views/Landing/InspectHistory.jsx
@@ -3,6 +3,9 @@ import { View, Text, ScrollView, Image } from 'react-native';
 import Iconn from 'react-native-vector-icons/MaterialCommunityIcons';
 import Style from '../Style';
 import icon1 from '../../assets/Icon/necessities/n1.png';
+import N from '../../assets/Icon/Icons/Necessities.png';
+import S from '../../assets/Icon/Icons/Savings.png';
+import W from '../../assets/Icon/Icons/Wants.png'
 
 const InspectHistory = ({route}) => {
   const {details} = route.params
@@ -15,7 +18,7 @@ const InspectHistory = ({route}) => {
           <Text style={{ color: '#144714', fontSize: 17, top: -5, alignSelf: 'center' }}>[ Predicted Average Per Month ]</Text>
           <View style={{ flexDirection: 'col', justifyContent: 'space-between' }}>
             {details.map((data,index)=>(<View key={index} style={{ backgroundColor: '#144714', borderRadius: 10, flexDirection: 'row', flex: 1,margin:5 }}>
-              <Image source={icon1} style={{ width: 80, height: 80, margin: 10, backgroundColor: '#3A6B35', borderRadius: 10 }} />
+              <Image source={data.key == "Necessities" ? N : data.key == "Wants" ? W : S } style={{ width: 80, height: 80, margin: 10, backgroundColor: '#3A6B35', borderRadius: 10 }} />
               <View style={{ flexDirection: 'column', alignSelf: 'center', marginRight: 10, borderLeftWidth: 2, borderColor: '#E3B448', backgroundColor: '#3A6B35', borderBottomRightRadius: 5, borderTopRightRadius: 5, flex: 1 }}>
                 <View style={{ paddingVertical: 10 }}>
                   <Text style={{ color: '#E3B448', marginLeft: 5, fontWeight: 'bold', width: 'auto' }}> {data.key} </Text>
