9203e7e1600684abf61f323ea48d06849ac4e055
Meljohnzer <meljohnzer.hamoc@gmail.com>
2023-12-04 17:16:34 +0800

    l

--- views/Landing/Home.jsx
+++ views/Landing/Home.jsx
@@ -10,7 +10,6 @@ import DonutChart from './DonutChart';
 import { axiosRequest } from '../../api_server/axios'
 import UserContext from '../../api_server/context';
 import YearPicker from '../YearPicker';
-import { ColorSpace } from 'react-native-reanimated';
 
 const Home = ({ navigation }) => {
 
@@ -128,36 +127,16 @@ const Home = ({ navigation }) => {
   const [availableYears, setAvailableYears] = useState([]);
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
 
-
-
-  const filterAPI  = () =>{
-    axiosRequest.get(`gabay/same/year/${context.id}/?year=${selectedYear}`)
-    .then((response) => {
-      const date = { ...response.data };
-      // setDdate(date);
-      // console.log(response.data)
-      // // Extract the unique years from the expenses data
-      const uniqueYears = Array.from(new Set(Object.keys(date).map((key) => new Date(date[key].date).getFullYear())));
-      setAvailableYears(uniqueYears);
-      
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-  }
-
-
-
   const api = () => {
     axiosRequest
       .get(`gabay/same/month/year/${context.id}/?year=${selectedYear}`)
       .then((response) => {
         const date = { ...response.data };
-        setDdate(date); 
-        // console.log("api",response.data)
-        // // Extract the unique years from the expenses data
-        // const uniqueYears = Array.from(new Set(Object.keys(date).map((key) => new Date(date[key].date).getFullYear())));
-        // setAvailableYears(uniqueYears);
+        setDdate(date);
+
+        // Extract the unique years from the expenses data
+        const uniqueYears = Array.from(new Set(Object.keys(date).map((key) => new Date(date[key].date).getFullYear())));
+        setAvailableYears(uniqueYears);
       })
       .catch((e) => {
         console.log(e);
@@ -169,13 +148,9 @@ const Home = ({ navigation }) => {
       .get(`gabay/page/${context.id}/?date=${date || null}&page=1&year=${selectedYear}`)
       .then((response) => {
         setExpense(response.data);
-        // console.log("ddate",ddate);
-        // console.log(ddate)
       })
       .catch((e) => {
         console.log(e);
-        setExpense(null)
-        console.log(ddate)
         setChartLoading(false);
       });
   };
@@ -222,39 +197,20 @@ const Home = ({ navigation }) => {
   
 
   useEffect(() => {
-
-
-
-    if(Object.keys(availableYears).length > 0){
-    api() 
-    }
-    
-    // if(Object.keys(availableYears).length > 0){
-    //   setSelectedYear(availableYears[0])
-      
-    // }
-    
-    
     const onFocus = async () => {
-      setPage(0)
-      if(Object.keys(availableYears).length > 0){
-        const avy = availableYears[0]
-        if(avy){
-          setSelectedYear(avy)
-      console.log("ass",ddate)
-        }else{setSelectedYear(availableYears[availableYears.length - 1])}
-      
-      
-    }
-      filterAPI()
+      api();
       getIncome()
+      setPage(0)
       // setPdfPrint(null)
       setDelay(true)
-      
+      const selectedDate = ddate[page]?.date || (ddate[0]?.date || null);
+      // if (selectedDate) {
+      //   getData(selectedDate);
+      // }
 
       setTimeout(() => {
         setIsLoading(false);
-      }
+      })
     };
 
     const unsubscribe = navigation.addListener('focus', onFocus);
@@ -262,24 +218,17 @@ const Home = ({ navigation }) => {
     return () => {
       unsubscribe();
     };
-  }, [ navigation,availableYears,selectedYear]);
-
+  }, [ navigation,context, page, expense]);
 
   useEffect(() => {
     const selectedDate = ddate[page]?.date || (ddate[0]?.date || null);
     if (selectedDate) {
       getData(selectedDate);
-        
-      // if(Object.keys(availableYears).length > 0){
-      //   setSelectedYear(availableYears[0])
-      //   console.log(availableYears[0])
-      // }
     }
-    // console.log(ddate)  
-           
 
-  }, [navigation,ddate,availableYears,selectedYear]);
-  
+
+  }, [page, ddate]);
+
   const toggleOption = () => {
     setSelectedOption(selectedOption === 'Income' ? 'Expenses' : 'Income');
   };
@@ -325,13 +274,12 @@ const Home = ({ navigation }) => {
             {selectedOption === 'Income' && (
 
               <View style={{ top: 5, backgroundColor: '#CBD18F', paddingHorizontal: 10, marginHorizontal: 10, borderRadius: 10, }}>
-               { expense && <YearPicker
+                 {/* <YearPicker
         selectedYear={selectedYear}
         onYearChange={setSelectedYear}
         years={availableYears}
-        onBlur={()=>setSelectedYear(selectedYear)}
-      /> }
-                {Object.keys(ddate).length && expense ? <View >
+      /> */}
+                {Object.keys(ddate).length ? (<View >
                   <View style={{ top: 10, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 10,textAlign:'center' }}>
                   
                   {Object.keys(ddate).length > 1 &&  <TouchableOpacity onPress={handlePresslef}>
--- views/YearPicker.jsx
+++ views/YearPicker.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Picker } from '@react-native-picker/picker';
 
-const YearPicker = ({ selectedYear, onYearChange, years ,onBlur}) => {
+const YearPicker = ({ selectedYear, onYearChange, years }) => {
   return (
     <Picker
       selectedValue={selectedYear}
       onValueChange={(itemValue, itemIndex) => onYearChange(itemValue)}
-      onBlur={onBlur}
     >
       {years.map((year) => (
         <Picker.Item key={year} label={year.toString()} value={year} />
